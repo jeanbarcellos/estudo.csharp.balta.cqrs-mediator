6e209b26c223936b424a290eacaf9f834492fde9
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-28 20:51:46 -0300

    Criação do Controller

---
+++ Controllers/CreateCustomerHandler.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Handlers;
+
+namespace Shop.Controllers
+{
+    [ApiController]
+    public class CreateCustomerHandler : ControllerBase
+    {
+        [HttpPost]
+        [Route("")]
+        public IActionResult Create(
+            [FromServices] ICreateCustomerHandler handler,
+            [FromBody] CreateCustomerRequest command
+        )
+        {
+            var response = handler.Handle(command);
+
+            return Ok(response);
+        }
+    }
+}

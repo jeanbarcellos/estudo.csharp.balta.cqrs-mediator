bf2d9bfd11b3a143d7434f955a1151fcbea34b8b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-28 23:19:38 -0300

    Adição de implementações em CreateCustomerHandler

--- Domain/Handlers/CreateCustomerHandler.cs
+++ Domain/Handlers/CreateCustomerHandler.cs
@@ -1,26 +1,43 @@
 using System;
+using Shop.Domain.Entities;
 using Shop.Domain.Commands.Requests;
 using Shop.Domain.Commands.Responses;
+using Shop.Domain.Repositories;
+using Shop.Domain.Services;
 
 namespace Shop.Domain.Handlers
 {
     public class CreateCustomerHandler : ICreateCustomerHandler
     {
+        ICustomerRepository _repository;
+        IEmailService _emailService;
+
+        public CreateCustomerHandler(ICustomerRepository repository, IEmailService emailService)
+        {
+            _repository = repository;
+            _emailService = emailService;
+        }
+
         public CreateCustomerResponse Handle(CreateCustomerRequest command)
         {
             // Verificar se o cliente já está cadastrado
 
             // Validar os dados
 
-            // Inserir o cliente
+            // Cria a entidade
+            var customer = new Customer(command.Name, command.Email);
 
-            // Enviar e-mail de boas vinda
+            // Persiste a entidade no banco
+            _repository.Save(customer);
+
+            // Envia E-mail de boas-vindas
+            _emailService.Send(customer.Name, customer.Email);
 
             return new CreateCustomerResponse
             {
-                Id = Guid.NewGuid(),
-                Name = "Jean Barcellos",
-                Email = "jeanbarcellos@teste.com",
+                Id = customer.Id,
+                Name = customer.Name,
+                Email = customer.Email,
                 Date = DateTime.Now
             };
         }

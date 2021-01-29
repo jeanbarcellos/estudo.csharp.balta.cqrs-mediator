2e36fa3e55905736035b8202d6856948215f73cb
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-28 23:55:20 -0300

    Criação de action no controller para a Query FindCustomerByIdRequest

--- Controllers/CreateCustomerHandler.cs
+++ Controllers/CreateCustomerHandler.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Shop.Domain.Commands.Requests;
 using Shop.Domain.Handlers;
+using Shop.Domain.Queries.Requests;
 
 namespace Shop.Controllers
 {
@@ -8,6 +9,19 @@ namespace Shop.Controllers
     [Route("customers")]
     public class CreateCustomerHandler : ControllerBase
     {
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetByid(
+            [FromServices] IFindCustomerByIdHandler handler,
+            [FromQuery] FindCustomerByIdRequest command
+        )
+        {
+            var result = handler.Handle(command);
+
+            return Ok(result);
+        }
+
         [HttpPost]
         [Route("")]
         public IActionResult Create(

e08bbfb436d38f763ca3b7d527d88c31ddf7b5be
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-28 22:36:18 -0300

    Criação da entidade Customer

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Shop.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public Customer(string name, string email)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+        }
+
+
+    }
+}

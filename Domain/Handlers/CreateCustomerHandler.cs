f244b697381397c2bce38e00731e8f89492b04db
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-01-30 10:08:22 -0300

    Refatorando os manipuladores

--- Domain/Handlers/CreateCustomerHandler.cs
+++ Domain/Handlers/CreateCustomerHandler.cs
@@ -4,10 +4,13 @@ using Shop.Domain.Commands.Requests;
 using Shop.Domain.Commands.Responses;
 using Shop.Domain.Repositories;
 using Shop.Domain.Services;
+using MediatR;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace Shop.Domain.Handlers
 {
-    public class CreateCustomerHandler : ICreateCustomerHandler
+    public class CreateCustomerHandler : IRequestHandler<CreateCustomerRequest, CreateCustomerResponse>
     {
         ICustomerRepository _repository;
         IEmailService _emailService;
@@ -18,14 +21,15 @@ namespace Shop.Domain.Handlers
             _emailService = emailService;
         }
 
-        public CreateCustomerResponse Handle(CreateCustomerRequest command)
+
+        public Task<CreateCustomerResponse> Handle(CreateCustomerRequest request, CancellationToken cancellationToken)
         {
             // Verificar se o cliente já está cadastrado
 
             // Validar os dados
 
             // Cria a entidade
-            var customer = new Customer(command.Name, command.Email);
+            var customer = new Customer(request.Name, request.Email);
 
             // Persiste a entidade no banco
             _repository.Save(customer);
@@ -33,13 +37,15 @@ namespace Shop.Domain.Handlers
             // Envia E-mail de boas-vindas
             _emailService.Send(customer.Name, customer.Email);
 
-            return new CreateCustomerResponse
+            var result = new CreateCustomerResponse
             {
                 Id = customer.Id,
                 Name = customer.Name,
                 Email = customer.Email,
                 Date = DateTime.Now
             };
+
+            return Task.FromResult(result);
         }
     }
 
--- Domain/Handlers/FindCustomerByIdHandler.cs
+++ Domain/Handlers/FindCustomerByIdHandler.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
 using Shop.Domain.Queries.Requests;
 using Shop.Domain.Queries.Responses;
 using Shop.Domain.Repositories;
 
 namespace Shop.Domain.Handlers
 {
-    public class FindCustomerByIdHandler : IFindCustomerByIdHandler
+    public class FindCustomerByIdHandler : IRequestHandler<FindCustomerByIdRequest, FindCustomerByIdResponse>
     {
         ICustomerRepository _repository;
 
@@ -14,21 +17,23 @@ namespace Shop.Domain.Handlers
             _repository = repository;
         }
 
-        public FindCustomerByIdResponse Handle(FindCustomerByIdRequest command)
+        public Task<FindCustomerByIdResponse> Handle(FindCustomerByIdRequest request, CancellationToken cancellationToken)
         {
-            var customer = _repository.GetCustomerById(command.Id);
+            var customer = _repository.GetCustomerById(request.Id);
 
             if (customer == null)
             {
                 throw new Exception("Nada");
             }
 
-            return new FindCustomerByIdResponse
+            var result = new FindCustomerByIdResponse
             {
                 Id = customer.Id,
                 Name = customer.Name,
                 Email = customer.Email,
             };
+
+            return Task.FromResult(result);
         }
     }
 }
